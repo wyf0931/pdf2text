3ea7fbeb9202bfa7a7deb5305abe181fedd34a56
wyf0931 <wyf0931@gmail.com>
2024-04-24 13:07:25 +0800

    init

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import os
 import json
 import hashlib
 import tempfile
-from flask import send_from_directory, render_template, Flask, jsonify, request
+import fitz
+import pytesseract
+import pdf2image
+from PIL import Image
+from flask import render_template, Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -36,11 +40,43 @@ def upload_file():
         'status': 0,
         'msg': '',
         'data': {
-            'value': f'/files/{file_hash}',
+            'value': f'{save_path}',
+            'text': extract(save_path),
             'name': file.filename
         }
     }
     return jsonify(response)
+
+
+def extract(file_path:str) -> str:
+    doc = fitz.open(file_path)
+    scanned_flag = []
+    for page_num in range(doc.page_count):
+        scanned_flag.append(is_page_scanned(doc, page_num))
+
+    images = pdf2image.convert_from_path(file_path)
+    text = ''
+    for page_num in range(doc.page_count):
+        try:
+            if scanned_flag[page_num]:
+                # ocr
+                image = images[page_num]
+                text += pytesseract.image_to_string(image, lang='chi_sim+eng')
+            else:
+                # extract
+                page = doc.load_page(page_num)
+                text += page.get_text()
+            print(f'extract page success, pdf_hash={file_path}, current_page_num={page_num}, is_scanned={scanned_flag[page_num]}')
+        except Exception as e:
+            print('extract pdf text fail.', stack_info=True)
+    doc.close()
+    return text
+
+
+def is_page_scanned(doc, page_num):
+    page = doc.load_page(page_num)
+    images = page.get_images()
+    return len(images) == 1
 
 @app.route('/')
 def index():
