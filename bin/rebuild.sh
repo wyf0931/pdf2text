258ea483d6b019a005a5b1931e82fc643073f9e0
wyf0931 <wyf0931@gmail.com>
2024-04-24 16:34:51 +0800

    init

--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -1,2 +1,9 @@
 echo 'pull latest code...'
 git pull
+
+echo 'activate venv'
+source venv/bin/activate
+
+echo 'start server'
+# gunicorn --workers 1 --threads 4 -b 0.0.0.0:5000 --timeout 120 main:app
+gunicorn --workers 1 --threads 4 -b 0.0.0.0:5000 --timeout 120 --env "file-path=/var/data/files/pdf2txt" main:app
