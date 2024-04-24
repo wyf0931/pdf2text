4dfaea6b374a370306eafe5551a1f1a6835e7b68
wyf0931 <wyf0931@gmail.com>
2024-04-24 22:26:54 +0800

    init

--- bin/start.sh
+++ bin/start.sh
@@ -3,4 +3,4 @@ source venv/bin/activate
 
 echo 'start server'
 # gunicorn --workers 1 --threads 4 -b 0.0.0.0:5000 --timeout 120 main:app
-gunicorn --workers 1 --threads 4 -b 0.0.0.0:5000 --timeout 120 --env "file-path=/var/data/files/pdf2txt" main:app
+nohup gunicorn --workers 1 --threads 4 -b 0.0.0.0:5000 --timeout 120 --env "file-path=/var/data/files/pdf2txt" main:app &
