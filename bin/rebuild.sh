756c0efee7f50168e209542b112f7900a17eefe6
wyf0931 <wyf0931@gmail.com>
2024-04-24 11:25:12 +0800

    init

--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -1,8 +1,2 @@
 echo 'pull latest code...'
 git pull
-
-echo 'activate venv'
-source venv/bin/activate
-
-echo 'start server'
-gunicorn --workers 1 --threads 4 -b 0.0.0.0:5000 --timeout 120 main:app
---
+++ bin/start.sh
@@ -0,0 +1,5 @@
+echo 'activate venv'
+source venv/bin/activate
+
+echo 'start server'
+gunicorn --workers 1 --threads 4 -b 0.0.0.0:5000 --timeout 120 main:app
