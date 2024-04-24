7365525e6776bc37bc969d75f78899702d3ccd75
wyf0931 <wyf0931@gmail.com>
2024-04-24 18:32:13 +0800

    init

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import fitz
 import pytesseract
 import pdf2image
 from PIL import Image
-from flask import render_template, Flask, jsonify, request
+from io import BytesIO
+import base64
+from flask import render_template, Flask, jsonify, request, Response
 
 app = Flask(__name__)
 
@@ -14,6 +16,7 @@ app = Flask(__name__)
 save_dir = os.getenv('file-path', tempfile.gettempdir())
 os.makedirs(save_dir, exist_ok=True)
 print(f'filepath=====>{save_dir}')
+
 
 @app.route('/api/upload', methods=['POST'])
 def upload_file():
@@ -38,17 +41,50 @@ def upload_file():
     # 返回JSON响应
     response = {
         'status': 0,
-        'msg': '',
+        'msg': 'ok',
         'data': {
-            'value': f'{save_path}',
-            'text': extract(save_path),
-            'name': file.filename
+            'code': file_hash
         }
     }
     return jsonify(response)
 
 
-def extract(file_path:str) -> str:
+@app.route('/api/convert/<file_hash>')
+def convert(file_hash):
+    file_path = os.path.join(save_dir, file_hash)
+
+    if not os.path.exists(file_path):
+        return jsonify({'status': -1, 'msg': 'File not found'})
+
+    doc = fitz.open(file_path)
+    images = pdf2image.convert_from_path(file_path)
+
+    def generate():
+        for page_num in range(doc.page_count):
+            scanned_flag = is_page_scanned(doc, page_num)
+
+            if scanned_flag:
+                # ocr
+                image = images[page_num]
+                text = pytesseract.image_to_string(image, lang='chi_sim+eng')
+            else:
+                # extract
+                page = doc.load_page(page_num)
+                text = page.get_text()
+
+            # 将文本编码为 base64
+            encoded_text = base64.b64encode(text.encode()).decode()
+
+            # 发送事件
+            yield 'data: {}\n\n'.format(encoded_text)
+
+        # 发送完成事件
+        yield 'data: finish\n\n'
+    # 返回事件流
+    return Response(generate(), mimetype='text/event-stream')
+
+
+def extract(file_path: str) -> str:
     doc = fitz.open(file_path)
     scanned_flag = []
     for page_num in range(doc.page_count):
@@ -66,7 +102,8 @@ def extract(file_path:str) -> str:
                 # extract
                 page = doc.load_page(page_num)
                 text += page.get_text()
-            print(f'extract page success, pdf_hash={file_path}, current_page_num={page_num}, is_scanned={scanned_flag[page_num]}')
+            print(f'extract page success, pdf_hash={file_path}, current_page_num={
+                  page_num}, is_scanned={scanned_flag[page_num]}')
         except Exception as e:
             print('extract pdf text fail.', stack_info=True)
     doc.close()
@@ -77,6 +114,7 @@ def is_page_scanned(doc, page_num):
     page = doc.load_page(page_num)
     images = page.get_images()
     return len(images) == 1
+
 
 @app.route('/')
 def index():
