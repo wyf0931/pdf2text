9f5aeab56296b418e6028434b04c97c6acc46018
wyf0931 <wyf0931@gmail.com>
2024-04-25 13:16:05 +0800

    init

--- bin/start.sh
+++ bin/start.sh
@@ -12,5 +12,11 @@ if [ -f ./pdf2text.pid ]; then
     echo '' > ./pdf2text.pid
 fi
 
-echo 'start server'
-nohup sh -c 'echo $! > ./pdf2text.pid; exec gunicorn --workers 1 --threads 4 -b 0.0.0.0:5000 --timeout 120 --env "file-path=/var/data/files/pdf2txt" main:app' &
+# 启动服务器，并将进程号写入 pid 文件
+nohup sh -c 'gunicorn --workers 1 --threads 4 -b 0.0.0.0:5000 --timeout 120 --env "file-path=/var/data/files/pdf2txt" main:app & echo $! > ./pdf2text.pid' &
+
+# 等待一秒确保进程已经启动
+sleep 1
+
+# 打印出启动的进程号
+echo "Server started with PID: $(cat ./pdf2text.pid)"
