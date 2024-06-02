e84bf3e3d8df504cbd01a87782f1fe996a1cb216
scott <yunfei.wang@shulex-tech.com>
2024-06-02 19:18:43 +0800

    update start

--- bin/start.sh
+++ bin/start.sh
@@ -1,22 +1,25 @@
+echo 'pull latest code...'
+git pull
+
 echo 'activate venv'
 source venv/bin/activate
 
 echo 'stop previous server if running'
 if [ -f ./pdf2text.pid ]; then
-    PID=$(cat ./pdf2text.pid)
+    PID=$(cat /var/run/xpdf/extractor.pid)
     if ps -p $PID > /dev/null; then
         echo "Stopping previous server with PID $PID"
         kill $PID
         sleep 1
     fi
-    echo '' > ./pdf2text.pid
+    echo '' > /var/run/xpdf/extractor.pid
 fi
 
 # 启动服务器，并将进程号写入 pid 文件
-nohup sh -c 'gunicorn --workers 1 --threads 4 -b 0.0.0.0:5000 --timeout 120 --env "file-path=/var/data/files/pdf2txt" main:app & echo $! > ./pdf2text.pid' &
+nohup sh -c 'gunicorn --workers 1 --threads 4 -b 0.0.0.0:5000 --timeout 120 --env "file-path=/var/cache/xpdf" main:app & echo $! > /var/run/xpdf/extractor.pid' &
 
 # 等待一秒确保进程已经启动
 sleep 1
 
 # 打印出启动的进程号
-echo "Server started with PID: $(cat ./pdf2text.pid)"
+echo "Server started with PID: $(cat /var/run/xpdf/extractor.pid)"
